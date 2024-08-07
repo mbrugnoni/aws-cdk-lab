02c2564b81d681eb1a129b50b4250d2f7aa3d69c
mbrugnoni <michael.brugnoni@gmail.com>
2024-08-07 00:20:38 -0400

    update

--- lab-cdk.py
+++ lab-cdk.py
@@ -1,9 +1,6 @@
+from aws_cdk import Stack
+from aws_cdk import aws_ec2 as ec2
 from constructs import Construct
-from aws_cdk import (
-    App,
-    Stack,
-    aws_ec2 as ec2,
-)
 
 class VPCStack(Stack):
 
